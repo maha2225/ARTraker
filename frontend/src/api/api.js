68a307592935a89b658b17b935d6f6f5f173bd66
maha2225 <mahasebri24@gmail.com>
2025-11-25 17:08:47 +0300

    Updated front end for deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-
+import { API_URL } from "../config.js";
 const API = axios.create({
-  baseURL: 'http://localhost:5000', // backend URL
+  baseURL: API_URL, 
 });
 
-// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
